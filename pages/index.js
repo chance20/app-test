ee6bfb405bff85066123f986e06a0f66bf3a968f
chance muteba <chancemuteba2020@gmail.com>
2021-10-18 22:18:35 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { Heading, Page , Button} from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>Welcome to Orderbot Software  🎉</Heading>
+    <Button></Button>
+  </Page>
+);
+
+export default Index;
